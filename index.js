acbccfb73287105a94cf61eac67738be689c1ec3
John Veldboom <jveldboom@gmail.com>
2018-12-16 21:26:14 -0500

    update to fix handful of linting issues

--- index.js
+++ index.js
@@ -1,41 +1,37 @@
 module.exports = {
 
-  debug(message, attributes) {
+  debug (message, attributes) {
     this.format(`debug`, message, attributes).write()
   },
 
-  info(message, attributes) {
+  info (message, attributes) {
     this.format(`info`, message, attributes).write()
   },
 
-  warn(message, attributes) {
+  warn (message, attributes) {
     this.format(`warning`, message, attributes).write()
   },
 
-  error(message, attributes) {
+  error (message, attributes) {
     this.format(`error`, message, attributes).write()
   },
 
-  alert(message, attributes) {
+  alert (message, attributes) {
     this.format(`alert`, message, attributes).write()
   },
 
-  format(level, message, attributes) {
-
+  format (level, message, attributes) {
     try {
-
       if (attributes instanceof Error) {
         attributes = {
           error: attributes.message,
           stack: attributes.stack
         }
-      }
-      // remove empty object
-      else if (attributes && !Object.keys(attributes).length) {
+      } else if (attributes && !Object.keys(attributes).length) {
+        // remove empty object
         attributes = undefined
-      }
-      // force attributes to be object
-      else if (typeof attributes === 'string') {
+      } else if (typeof attributes === 'string') {
+        // force attributes to be object
         attributes = { string: attributes }
       }
 
@@ -44,9 +40,7 @@ module.exports = {
         message: String(message),
         attributes
       })
-
     } catch (err) {
-
       // catch circular reference and other exception
       attributes = { logError: err.message }
       this.logLine = JSON.stringify({
@@ -59,7 +53,7 @@ module.exports = {
     return this
   },
 
-  write() {
+  write () {
     console.log('%s', this.logLine)
   }
 }
